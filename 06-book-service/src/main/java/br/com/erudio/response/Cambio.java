ba4b00c8e778b3ab50ae4416aab3c9ff022a140f
Johnathan Rodrigues <deadjhow@gmail.com>
2022-10-07 08:51:27 -0300

    exemplos do curso de microsserviços

---
+++ 06-book-service/src/main/java/br/com/erudio/response/Cambio.java
@@ -0,0 +1,86 @@
+package br.com.erudio.response;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Cambio implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String from;
+	private String to;
+	private Double conversionFactor;
+	private Double convertedValue;
+	private String environment;
+	
+	public Cambio() {};
+	
+	public Cambio(Long id, String from, String to, 
+			Double conversionFactor, Double convertedValue,
+			String environment) {
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionFactor = conversionFactor;
+		this.convertedValue = convertedValue;
+		this.environment = environment;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public Double getConversionFactor() {
+		return conversionFactor;
+	}
+	public void setConversionFactor(Double conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
+	public Double getConvertedValue() {
+		return convertedValue;
+	}
+	public void setConvertedValue(Double convertedValue) {
+		this.convertedValue = convertedValue;
+	}
+	public String getEnviroment() {
+		return environment;
+	}
+	public void setEnviroment(String environment) {
+		this.environment = environment;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(conversionFactor, convertedValue, from, id, environment, to);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cambio other = (Cambio) obj;
+		return Objects.equals(conversionFactor, other.conversionFactor)
+				&& Objects.equals(convertedValue, other.convertedValue) && Objects.equals(from, other.from)
+				&& Objects.equals(id, other.id) && Objects.equals(environment, other.environment)
+				&& Objects.equals(to, other.to);
+	}
+	
+	
+	
+}
